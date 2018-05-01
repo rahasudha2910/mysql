1b0c234aa202d1905ef8d0c9094a4bbdcb62edd9
rahasudha2910 <37336116+rahasudha2910@users.noreply.github.com>
2018-05-01 10:17:21 +0530

    Create and or.sql

---
+++ hunter/and or.sql
@@ -0,0 +1,14 @@
+create database sudha;
+show databases;
+use sudha;
+create table student(id int(2),name char(25),age int(2));
+desc student;
+insert into student values("1","Vinodini","25");
+insert into student values("2","Banu","27");
+insert into student values("3","kaushik","23");
+insert into student values("4","Praveen","25");
+insert into student values("5","kamal","22");
+insert into student values("6","Malini","24");
+insert into student values("7","Ramesh","32");
+select * from student;
+select * from student where age="32" or age="22";
